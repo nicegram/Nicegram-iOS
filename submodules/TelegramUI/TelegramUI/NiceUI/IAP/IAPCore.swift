12c2fc6c8c6d3fb583bed711df37de2ec3d68775
Kylmakalle <iceman9831@gmail.com>
2019-08-05 18:24:12 +0300

    iAP stubs

---
+++ submodules/TelegramUI/TelegramUI/NiceUI/IAP/IAPCore.swift
@@ -0,0 +1,13 @@
+//
+//  IAPCore.swift
+//  TelegramUI
+//
+//  Created by Sergey on 05/08/2019.
+//  Copyright © 2019 Nicegram. All rights reserved.
+//
+
+import Foundation
+
+struct IAPID {
+    let unlimFolders = "app.nicegram.iap.uFolders"
+}
---
+++ submodules/TelegramUI/TelegramUI/NiceUI/IAP/IAPListController.swift
@@ -0,0 +1,9 @@
+//
+//  IAPListController.swift
+//  TelegramUI
+//
+//  Created by Sergey on 05/08/2019.
+//  Copyright © 2019 Telegram. All rights reserved.
+//
+
+import Foundation
