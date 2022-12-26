ff0c88eb5e6ffe80b2a854be966c8856b777dd5f
Denis Shilovich <denis.shilovichhh@gmail.com>
2022-12-26 11:54:40 +0000

    1.1.7 (83)

--- Nicegram/NGLotteryUI/Sources/Presentation/Splash/Views/SplashSectionView.swift
+++ Nicegram/NGLotteryUI/Sources/Presentation/Splash/Views/SplashSectionView.swift
@@ -100,11 +100,15 @@ private extension SplashSectionView {
         button.applyLotteryActionStyle()
         
         let topStack = UIStackView(
-            arrangedSubviews: [titleImageView, titleLabel, badgeLabel],
+            arrangedSubviews: [titleImageView, titleLabel, UIView(), badgeLabel],
             axis: .horizontal,
             spacing: 8,
             alignment: .center
         )
+        topStack.setCustomSpacing(0, after: titleLabel)
+        titleImageView.snp.makeConstraints { make in
+            make.size.equalTo(29)
+        }
         titleLabel.snp.contentHuggingHorizontalPriority = 249
         titleLabel.snp.contentCompressionResistanceHorizontalPriority = 749
         
