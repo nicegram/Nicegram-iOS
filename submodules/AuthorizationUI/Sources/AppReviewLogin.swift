49b19558417bb04a3773e74258739b28e63163e5
Denis Shilovich <denis.shilovichhh@gmail.com>
2023-07-04 13:19:26 +0000

    1.3.6 (147)

--- submodules/AuthorizationUI/Sources/AppReviewLogin.swift
+++ submodules/AuthorizationUI/Sources/AppReviewLogin.swift
@@ -1,4 +1,6 @@
+import NGEnv
+
 struct AppReviewLogin {
-    static let phone = "9996638671"
-    static let code = "33333"
+    static let phone = NGENV.app_review_login_phone
+    static let code = NGENV.app_review_login_code
 }
