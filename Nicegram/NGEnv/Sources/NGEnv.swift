76aaf488713c1cc71519490919e3528bcc34a271
Denis Shilovich <denis.shilovichhh@gmail.com>
2024-10-02 18:59:40 +0000

    1.8.5 (331)

--- Nicegram/NGEnv/Sources/NGEnv.swift
+++ Nicegram/NGEnv/Sources/NGEnv.swift
@@ -25,8 +25,6 @@ public struct NGEnvObj: Decodable {
         public let web3AuthBackupQuestion: String
         public let web3AuthClientId: String
         public let web3AuthVerifier: String
-        public let stonfiApiUrl: String
-        public let stonfiNicegramApiUrl: String
     }
 }
 
