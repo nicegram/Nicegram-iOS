88b010e0911234a0afe93588e92cc6c7102c30a4
Denis Shilovich <denis.shilovichhh@gmail.com>
2024-12-16 18:49:35 +0000

    1.9.3 (397)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "nicegram-package",
     dependencies: [
-        .package(url: "git@bitbucket.org:mobyrix/nicegram-assistant-ios.git", branch: "develop"),
-        .package(url: "git@bitbucket.org:mobyrix/nicegram-wallet-ios.git", branch: "develop")
+        .package(url: "git@bitbucket.org:mobyrix/nicegram-assistant-ios.git", branch: "feat/solana"),
+        .package(url: "git@bitbucket.org:mobyrix/nicegram-wallet-ios.git", branch: "feat/solana")
     ]
 )
